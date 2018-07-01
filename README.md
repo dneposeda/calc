091db677095ea30a0df6a0d3b7186434d6580930
Denis <den.neposeda@gmail.com>
2018-07-02 00:08:48 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Калькулятор
+Посмотреть работу можно по ссылке - <a href="https://test.teatlt.ru/calc/">https://test.teatlt.ru/calc/</a>
---
+++ src/js/index.js
@@ -0,0 +1,133 @@
+import 'normalize.css';
+import '../css/style.css';
+
+
+var doc = document,
+    btnChengeTheme = doc.getElementById('btnChengeTheme'),
+    keyNumbers = doc.querySelectorAll('.keynumber'),
+    keyOperations = doc.querySelectorAll('.keyoperations'),
+    keyAc = doc.getElementById('keyac'),
+    keyDot = doc.getElementById('keydot'),
+    display = doc.getElementById('display'),
+    memoryCurrentNumber = 0,
+    memoryOperation = '',
+    entryNewNumber = false;
+
+
+
+function ChengeTheme(){
+    
+    var calc = doc.getElementById('calc');
+    calc.classList.toggle('dark');
+    calc.classList.toggle('light');
+
+};
+
+
+
+function pressKeyNumber(numb){
+
+    if (entryNewNumber){
+        display.value = numb;
+        entryNewNumber = false;
+    } else {
+        if (display.value === '0'){
+            display.value = numb;
+        } else {
+            display.value += numb;
+        };
+    };
+    
+};
+
+
+
+function pressKeyOperation(symbol){
+    
+    var localMemoryNumber = display.value;
+    
+    if (entryNewNumber && memoryOperation !== '='){
+        display.value = memoryCurrentNumber;
+        memoryOperation = symbol;
+    } else {
+        entryNewNumber = true;
+        switch(memoryOperation){
+            case '+':
+                memoryCurrentNumber += parseFloat(localMemoryNumber);
+                break;
+            case '-':
+                memoryCurrentNumber -= parseFloat(localMemoryNumber);
+                break;
+            case '*':
+                memoryCurrentNumber *= parseFloat(localMemoryNumber);
+                break;
+            case '/':
+                memoryCurrentNumber /= parseFloat(localMemoryNumber);
+                break;
+            default:
+                memoryCurrentNumber = parseFloat(localMemoryNumber);
+        };
+        
+        display.value = +memoryCurrentNumber.toFixed(10);
+        memoryOperation = symbol;
+    };
+
+};
+
+
+
+function pressKeyDot(dot){
+
+    var localMemoryDot = display.value;
+
+    if (entryNewNumber){
+        localMemoryDot = '0.';
+        entryNewNumber = false;
+    } else {
+        if (localMemoryDot.indexOf('.') === -1){
+            localMemoryDot += '.';
+        };
+    };
+
+    display.value = localMemoryDot;
+
+};
+
+
+
+function pressKeyClear(){
+    display.value = 0;
+    memoryCurrentNumber = 0;
+    memoryOperation = '';
+    entryNewNumber = true;
+    
+};
+
+
+
+/* События на клик смены темы */
+btnChengeTheme.addEventListener('click', ChengeTheme);
+
+/* События на клик по цифрам */
+for (var i = 0; i<keyNumbers.length; i++){
+    var keyNumber = keyNumbers[i];
+    keyNumber.addEventListener('click', function(e){
+        pressKeyNumber(e.target.textContent);
+    });
+}
+
+/* События на клик по операциям */
+for (var i = 0; i<keyOperations.length; i++){
+    var keyOperation = keyOperations[i];
+    keyOperation.addEventListener('click', function(e){
+        pressKeyOperation(e.target.textContent);
+    });
+}
+
+/* События на клик очистки */
+keyAc.addEventListener('click', pressKeyClear);
+
+/* События на клик точки */
+keyDot.addEventListener('click', function(e){
+    pressKeyDot(e.target.textContent);
+});
