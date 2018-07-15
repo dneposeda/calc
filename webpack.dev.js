408dcba16dc4ba564549ba2f462d90f3e3ea9662
Denis <den.neposeda@gmail.com>
2018-07-15 17:54:16 +0400

    adding two webpack configurations

---
+++ webpack.dev.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+var clientConfig = (function webpackConfig(){
+
+  var config = Object.assign({});
+
+  config.mode = 'development';
+
+  config.entry = './src/js/index.js';
+
+  config.output = {
+    filename: 'js/script.js',
+    path: path.resolve(__dirname, 'dist/dev')
+  };
+
+  config.module = {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader'
+          }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+        			options: { 
+                minimize: false,
+                url: false,
+                sourceMap: true
+              }
+          },
+          {
+            loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+          }
+        ]
+      }
+
+    ]
+  };
+
+  config.devtool = 'source-map';
+
+  config.resolve = {};
+
+  config.plugins = [];
+
+  config.plugins.push(
+    new HtmlWebpackPlugin({
+      h1: 'development',
+	    template: 'src/template/index.html'
+    })
+  );
+
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    })
+  );
+
+  config.plugins.push(
+    new CopyWebpackPlugin([
+      {
+        from: 'assets/img',
+        to: 'assets/img/[name].[ext]',
+      }
+    ])
+  );
+
+  config.plugins.push(
+    new CopyWebpackPlugin([
+      {
+        from: 'assets/fonts',
+        to: 'assets/fonts/[name].[ext]',
+      }
+    ])
+  );
+
+  return config;
+
+});
+
+module.exports = clientConfig;
